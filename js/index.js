424055ba80666dc07e9ea1672db76368d197e77b
shubh1996 <shubhmehta1996@gmail.com>
2017-04-14 10:57:56 +0530

    init commit

---
+++ js/index.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function(){
+alert('hi');
+
+});
+
+// if (!('Notification' in window)) {
+//   console.log('This browser does not support notifications!');
+//   return;
+// }
+// Notification.requestPermission(function(status) {
+//   console.log('Notification permission status:', status);
+// });
+
+// displayNotification = function(){
+
+// if (Notification.permission == 'granted') {
+//   navigator.serviceWorker.getRegistration().then(function(reg) {
+
+//     // TODO 2.4 - Add 'options' object to configure the notification
+//     var options = {
+//   body: 'Notifications!!! Cool noe??',
+//   icon: 'images/favicon.png',
+//   vibrate: [100, 50, 100],
+//   data: {
+//     primaryKey: 1
+//   }
+// };
+
+//     reg.showNotification('SHUBH',options);
+//   });
+// }
+// };
+//  noti = function(){
+//   displayNotification();
+//   };
+// });
+
+// (function(){
+//       if(!'serviceWorker' in navigator)
+//       {
+//         alert('Service Worker not supported. Use a different browser');
+//         return;
+//       }
+//       navigator.serviceWorker.register('serviceWorker.js')
+//       .then(function(registration){
+//         console.log('Registered Service Worker');
+//       })
+//       .catch(function(error){
+//         console.log('Registration Failed. Error - '+error);
+//       });
+// })();
